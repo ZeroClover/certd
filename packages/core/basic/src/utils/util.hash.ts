44d43f45cb9094619df7494c2a64a51ba77ad116
xiaojunnuo <xiaojunnuo@qq.com>
2025-02-24 18:07:08 +0800

    perf: 支持新版本LeCDN

--- packages/core/basic/src/utils/util.hash.ts
+++ packages/core/basic/src/utils/util.hash.ts
@@ -6,7 +6,12 @@ function md5(data: string) {
 function sha256(data: string) {
   return crypto.createHash('sha256').update(data).digest('hex');
 }
+
+function base64(data: string) {
+  return Buffer.from(data).toString('base64');
+}
 export const hashUtils = {
   md5,
   sha256,
+  base64,
 };
