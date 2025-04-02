04d79f9117670be504960b018fd49ae3bf7c1c11
xiaojunnuo <xiaojunnuo@qq.com>
2025-04-02 23:13:55 +0800

    perf: 支持京东云dns申请证书

---
+++ packages/libs/lib-jdcloud/src/index.ts
@@ -0,0 +1,7 @@
+import jdCloud  from "./lib/core.js";
+import jdService from './lib/service.js'
+
+import domainService from './repo/domainservice/v2/domainservice.js'
+export const JDCloud = jdCloud;
+export const JDService = jdService;
+export const JDDomainService = domainService;
